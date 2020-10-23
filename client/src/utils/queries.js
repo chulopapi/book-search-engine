a660c9c08608598dd9dd9d3108e7e1b1ca2627c4
chulopapi <marlon.guandique@gmail.com>
2020-10-22 23:04:40 -0700

    adding queries to store all graphql query request

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,23 @@
 //Written by MG
+
+// storing GraphQL query 
+
+import gql from 'graphql-tag';
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
