8b92d013871ed28a4948caeac370c5e991fbdf07
chulopapi <marlon.guandique@gmail.com>
2020-10-26 18:51:16 -0700

    adding heroku configuration

--- server/server.js
+++ server/server.js
@@ -47,6 +47,6 @@ db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`🌍 Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
